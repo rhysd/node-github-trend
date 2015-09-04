43182f1f84fd91eaf2725f34add8d0e9ac98bda8
rhysd <lin90162@yahoo.co.jp>
2015-09-04 19:55:08 +0900

    remove unused package

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,28 +1,2 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-declare module GitHub {
-    interface APIConfig {
-        version: string;
-    }
-
-    interface GetAPIMessage {
-        user: string;
-        repo: string;
-        headers?: Object;
-    }
-
-    interface GetAPI {
-        get(msg: GetAPIMessage, callback: (err: Error, data: Object) => void): void;
-    }
-
-    class GitHubClient {
-        repos: GetAPI;
-
-        constructor(config: APIConfig);
-    }
-}
-
-declare module "github" {
-    var gh: typeof GitHub.GitHubClient;
-    export = gh;
-}
