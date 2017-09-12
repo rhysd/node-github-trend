8681d396a2480819d1b2a0df9c081e029e268f7b
rhysd <lin90162@yahoo.co.jp>
2017-09-12 21:00:28 +0900

    contain old type definitions not to break build

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="cheerio/cheerio.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="js-yaml/js-yaml.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="request/request.d.ts" />
