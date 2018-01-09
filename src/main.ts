f879aa1bef2498c982ab72f8fbb27fd7ae171fec
rhysd <lin90162@yahoo.co.jp>
2018-01-09 23:11:54 +0900

    typing an API response from GitHub repository API seriously

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,111 @@ export interface RepositoryEntry {
     name: string;
 }
 
+export interface APIOwnerOrOrg {
+    login: string;
+    id: number;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+}
+
+export interface APILicense {
+    key: string;
+    name: string;
+    spdx_id: string | null;
+    url: string | null;
+}
+
 export interface APIRepository {
-    [key: string]: any;
+    id: number;
+    name: string;
+    full_name: string;
+    owner: APIOwnerOrOrg;
+    private: boolean;
+    html_url: string;
+    description: string;
+    fork: boolean;
+    url: string;
+    forks_url: string;
+    keys_url: string;
+    collaborators_url: string;
+    teams_url: string;
+    hooks_url: string;
+    issue_events_url: string;
+    events_url: string;
+    assignees_url: string;
+    branches_url: string;
+    tags_url: string;
+    blobs_url: string;
+    git_tags_url: string;
+    git_refs_url: string;
+    trees_url: string;
+    statuses_url: string;
+    languages_url: string | null;
+    stargazers_url: string;
+    contributors_url: string;
+    subscribers_url: string;
+    subscription_url: string;
+    commits_url: string;
+    git_commits_url: string;
+    comments_url: string;
+    issue_comment_url: string;
+    contents_url: string;
+    compare_url: string;
+    merges_url: string;
+    archive_url: string;
+    downloads_url: string;
+    issues_url: string;
+    pulls_url: string;
+    milestones_url: string;
+    notifications_url: string;
+    labels_url: string;
+    releases_url: string;
+    deployments_url: string;
+    created_at: string;
+    updated_at: string;
+    pushed_at: string;
+    git_url: string;
+    ssh_url: string;
+    clone_url: string;
+    svn_url: string;
+    homepage: string | null;
+    size: number;
+    stargazers_count: number;
+    watchers_count: number;
+    language: string | null;
+    has_issues: boolean;
+    has_projects: boolean;
+    has_downloads: boolean;
+    has_wiki: boolean;
+    has_pages: boolean;
+    forks_count: number;
+    mirror_url: string | null;
+    archived: boolean;
+    open_issues_count: number;
+    license: APILicense | null;
+    forks: number;
+    open_issues: number;
+    watchers: number;
+    default_branch: string;
+    organization?: APIOwnerOrOrg;
+    network_count: number;
+    subscribers_count: number;
+
+    // Additional props
+    readme_url?: string;
 }
 
 export interface LangsRepositories {
@@ -312,7 +415,7 @@ export class Client {
         });
     }
 
-    fetchAppendingReadme(repo: { [key: string]: any }) {
+    fetchAppendingReadme(repo: APIRepository) {
         const readme_url = `${repo.html_url}/blob/${repo.default_branch}/README.md`;
         const opts: request.Options = {
             url: readme_url,
